ee1475d6b4fd593dfebc9c42a8edcd262f759b90
Christopherles <lestinchristopher@gmail.com>
2025-07-05 09:03:30 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -17,4 +17,22 @@ onload = () => {
       closeBtn.style.display = "none";
       openBtn.style.display = "inline-block";
   });
-  
+  onload = () => {
+  document.body.classList.remove("container");
+};
+
+const wrapper = document.querySelector(".wrapper");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.addEventListener("click", () => {
+  wrapper.classList.add("open");
+  openBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+});
+
+closeBtn.addEventListener("click", () => {
+  wrapper.classList.remove("open");
+  closeBtn.style.display = "none";
+  openBtn.style.display = "inline-block";
+});
