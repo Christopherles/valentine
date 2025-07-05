db2a6b6aa2b43afba2279a8aa70c35ca7dc65f92
Christopherles <lestinchristopher@gmail.com>
2025-07-05 09:22:44 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,38 +1,22 @@
-onload = () => {
-    document.body.classList.remove("container");
-  };
-
-  const wrapper = document.querySelector(".wrapper");
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  
-  openBtn.addEventListener("click", () => {
-      wrapper.classList.add("open");
-      openBtn.style.display = "none";
-      closeBtn.style.display = "inline-block";
-  });
-  
-  closeBtn.addEventListener("click", () => {
-      wrapper.classList.remove("open");
-      closeBtn.style.display = "none";
-      openBtn.style.display = "inline-block";
-  });
-  onload = () => {
+window.onload = () => {
   document.body.classList.remove("container");
 };
 
 const wrapper = document.querySelector(".wrapper");
+const letter = document.querySelector(".letter");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 
 openBtn.addEventListener("click", () => {
   wrapper.classList.add("open");
+  letter.classList.add("show");
   openBtn.style.display = "none";
   closeBtn.style.display = "inline-block";
 });
 
 closeBtn.addEventListener("click", () => {
   wrapper.classList.remove("open");
+  letter.classList.remove("show");
   closeBtn.style.display = "none";
   openBtn.style.display = "inline-block";
 });
